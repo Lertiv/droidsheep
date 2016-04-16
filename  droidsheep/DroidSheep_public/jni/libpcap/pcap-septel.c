aabc530705ec594252c049ba54bcdc5d385dccf3
Adrian Sieber <mail@adriansieber.com>
2016-04-16 10:39:14 +0000

    Fix typos in pcap-septel.c

---  droidsheep/DroidSheep_public/jni/libpcap/pcap-septel.c
+++  droidsheep/DroidSheep_public/jni/libpcap/pcap-septel.c
@@ -68,7 +68,7 @@ static void septel_platform_close(pcap_t *p) {
 /*
  *  Read at most max_packets from the capture queue and call the callback
  *  for each of them. Returns the number of packets handled, -1 if an
- *  error occured, or -2 if we were told to break out of the loop.
+ *  error occurred, or -2 if we were told to break out of the loop.
  */
 static int septel_read(pcap_t *p, int cnt, pcap_handler callback, u_char *user) {
 
